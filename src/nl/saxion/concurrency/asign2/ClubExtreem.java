942d2339be9be2c1afe3dde30cd288140c7c1f1e
Daan <499588@student.saxion.nl>
2021-09-26 13:11:29 +0200

    Join club and leave club

--- src/nl/saxion/concurrency/asign2/ClubExtreem.java
+++ src/nl/saxion/concurrency/asign2/ClubExtreem.java
@@ -9,6 +9,11 @@ public class ClubExtreem {
 
     private final int maxUsers, consecutiveRepresentatives;
 
+    private ReentrantLock lock = new ReentrantLock();
+
+    Condition emptyQueues = lock.newCondition();
+    Condition clubIsFull = lock.newCondition();
+
     private int currentUsersInClub = 0;
     private int representativeCounter = 0;
 
@@ -24,13 +29,61 @@ public class ClubExtreem {
     public void joinQueue(BaseVisitor visitor){
         if(visitor instanceof Visitor){
             waitingVisitors.add((Visitor) visitor);
+            emptyQueues.signal();
         }
         else if(visitor instanceof Representative){
             waitingRepresentatives.add((Representative) visitor);
+            emptyQueues.signal();
         }
         assert false;
-    }
+    };
 
+    public void joinClub(Queue<Representative> waitingRepresentatives, Queue<Visitor> waitingVisitors){
+        lock.lock();
+        try {
+            //if there are no waiting visitors wait
+            while (waitingRepresentatives.size() == 0 && waitingVisitors.size() == 0) {
+                emptyQueues.await();
+            }
+            //if there are representatives join the club
+            if (waitingRepresentatives.size() != 0 && representativeCounter != 3) {
+                while (currentUsersInClub == 10) {
+                    clubIsFull.await();
+                }
+                representativeCounter++;
+                waitingRepresentatives.remove();
+            //if there are regular visitors join the club
+            } else {
+                while (currentUsersInClub == 20) {
+                    clubIsFull.await();
+                }
+                currentUsersInClub++;
+                representativeCounter =0;
+                waitingVisitors.remove();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void leaveClub(BaseVisitor visitor){
+        lock.lock();
+
+        try{
+            while (visitor.timeInClub){
+                //wait
+            }
+            if(visitor.timeInClub == 0){
+                currentUsersInClub--;
+                clubIsFull.signal();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }finally {
+            lock.unlock();
+        }
+    };
 
 
 }
